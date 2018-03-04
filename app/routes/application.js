a4c96ac60f6d602dbdd5f16b66b02896066968cc
Waihon Yew <yewwaihon@gmail.com>
2018-03-04 15:00:16 +0800

    List books

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
+import db from '../utils/db';
 
 export default Route.extend({
+  model() {
+    return db.books();
+  }
 });
---
+++ app/utils/db.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Object.create({
+  _records: [
+    { title: "El Aleph" },
+    { title: "2666" },
+    { title: "The Black Swan" }
+  ],
+
+  books() {
+    return Ember.copy(this._records);
+  },
+
+  addBook(book) {
+    this._records.push(book);
+  },
+
+  removeBook(book) {
+    const index = this._records.indexOf(book);
+    // Remove the records at position `index`
+    this._records.splice(index, 1);
+  }
+});
